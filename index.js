669799ceb5c0bfff5b8dfde34820e477b2675d93
Aftab mohammed k <kizhisery@gmail.com>
2023-02-18 17:59:13 +0000

    modified:   index.js 	modified:   url.js

--- index.js
+++ index.js
@@ -1,104 +1,90 @@
 const axios = require("axios");
 const { url, header } = require("./url");
 const fun = async () => {
-  // const data = await axios(url, header);
-  // const res = data.data.data;
+  const data = await axios(url, header);
+  const res = data.data.data;
   let cr = 10 ** 7;
-  
-  let datas = [
-    {
-      symbol: 'CYIENT',
-      company: 'Cyient Limited',
-      anex: '7(2)',
-      acqName: 'Anand Parameswaran',
-      date: '31-May-2020 21:00',
-      pid: '1072833',
-      tkdAcqm: null,
-      buyValue: '0',
-      sellValue: '0',
-      buyQuantity: '0',
-      sellquantity: '0',
-      secType: 'Equity Shares',
-      secAcq: '24900',
-      did: '287900',
-      tdpTransactionType: 'Buy',
-      tdpDerivativeContractType: '-',
-      xbrl: 'https://archives.nseindia.com/corporate/xbrl/IT_1072833_262838_31052020090047_WEB.xml',
-      personCategory: 'Employees/Designated Employees',
-      befAcqSharesNo: '50168',
-      befAcqSharesPer: '0.05',
-      secVal: '100',
-      securitiesTypePost: 'Equity Shares',
-      afterAcqSharesNo: '75068',
-      afterAcqSharesPer: '0.07',
-      acqfromDt: '25-May-2020',
-      acqtoDt: '27-May-2020',
-      intimDt: '29-May-2020',
-      acqMode: 'Market Purchase',
-      derivativeType: '-',
-      exchange: 'NSE',
-      remarks: '-'
-    },
-    {
-      symbol: 'CYIENT',
-      company: 'Cyient Limited',
-      anex: '7(2)',
-      acqName: 'Anand Parameswaran',
-      date: '31-May-2020 21:00',
-      pid: '1072833',
-      tkdAcqm: null,
-      buyValue: '0',
-      sellValue: '0',
-      buyQuantity: '0',
-      sellquantity: '0',
-      secType: 'Equity Shares',
-      secAcq: '24900',
-      did: '287900',
-      tdpTransactionType: 'Sell',
-      tdpDerivativeContractType: '-',
-      xbrl: 'https://archives.nseindia.com/corporate/xbrl/IT_1072833_262838_31052020090047_WEB.xml',
-      personCategory: 'Employees/Designated Employees',
-      befAcqSharesNo: '50168',
-      befAcqSharesPer: '0.05',
-      secVal: '100',
-      securitiesTypePost: 'Equity Shares',
-      afterAcqSharesNo: '75068',
-      afterAcqSharesPer: '0.07',
-      acqfromDt: '25-May-2020',
-      acqtoDt: '27-May-2020',
-      intimDt: '29-May-2020',
-      acqMode: 'Market Sale',
-      derivativeType: '-',
-      exchange: 'NSE',
-      remarks: '-'
-    },
-  ]
-  
-  
-  const res = datas;
-  
-  
-  const val = res.reduce((acc, cur) => {
-    // acc[cur.symbol] = { secValue: 0, plegeValue: 0, pledgeInvoke: 0, ESOP: 0, secAcq: 0 }
-    if (!acc[cur.symbol]) {
-      acc[cur.symbol] = { secValue :0 , secAcq :0}
-      console.log(cur.acqMode)
-      if(cur.acqMode == 'Market Purchase' ){
-        // acc[cur.symbol].secValue += +cur.secVal
-        // acc[cur.symbol].secAcq += +cur.secAcq
-        acc[cur.symbol] = {...acc[cur.symbol],secValue:+cur.secVal,secAcq:+cur.secAcq}
-      }
-    }
 
+
+  const val = res.reduce((acc, cur) => {
+    // acc[cur.symbol] = { secValue: 0, plegeValue: 0, plegeValue: 0, ESOP: 0, secAcq: 0 }
     if (acc[cur.symbol]) {
-      if(cur.acqMode == 'Market Sale' ){
-        // acc[cur.symbol].secValue += -cur.secVal
-        // acc[cur.symbol].secAcq += -cur.secAcq
-        acc[cur.symbol] = {...acc[cur.symbol],secValue:acc[cur.symbol].secValue -= +cur.secVal,
-          secAcq:acc[cur.symbol].secAcq += +cur.secAcq}
+      switch (cur.acqMode) {
+        case 'Market Purchase':
+          acc[cur.symbol] = {
+            ...acc[cur.symbol], secValue: acc[cur.symbol].secValue += +cur.secVal / cr,
+            secAcq: acc[cur.symbol].secAcq += +cur.secAcq
+          }
+          break;
+        case 'Market Sale':
+          acc[cur.symbol] = {
+            ...acc[cur.symbol], secValue: acc[cur.symbol].secValue += -cur.secVal / cr,
+            secAcq: acc[cur.symbol].secAcq += +cur.secAcq
+          }
+          break;
+        case "Pledge Creation":
+          acc[cur.symbol] = {
+            ...acc[cur.symbol], plegeValue: acc[cur.symbol].plegeValue += +cur.secVal / cr,
+            secAcq: acc[cur.symbol].secAcq += +cur.secAcq
+          }
+          break;
+        case "Revokation of Pledge":
+          acc[cur.symbol] = {
+            ...acc[cur.symbol], plegeValue: acc[cur.symbol].plegeValue += -cur.secVal / cr,
+            secAcq: acc[cur.symbol].secAcq += +cur.secAcq
+          }
+          break;
+        case "Invocation of pledge":
+          acc[cur.symbol] = {
+            ...acc[cur.symbol], pledgeInvoke: acc[cur.symbol].pledgeInvoke += +cur.secVal / cr,
+            secAcq: acc[cur.symbol].secAcq += +cur.secAcq
+          }
+        case "ESOP": switch (cur.tdpTransactionType) {
+          case 'Buy':
+            acc[cur.symbol] = {
+              ...acc[cur.symbol], ESOP: acc[cur.symbol].pledgeInvoke += +cur.secVal / cr,
+              secAcq: acc[cur.symbol].secAcq += +cur.secAcq
+            }
+            break;
+          case 'Sell':
+            acc[cur.symbol] = {
+              ...acc[cur.symbol], ESOP: acc[cur.symbol].pledgeInvoke += -cur.secVal / cr,
+              secAcq: acc[cur.symbol].secAcq += +cur.secAcq
+            }
+            break;
+        }
+
       }
     }
-      
+    if (!acc[cur.symbol]) {
+      // acc[cur.symbol] = { secValue: 0, secAcq: 0 }
+      acc[cur.symbol] = { secValue: 0, plegeValue: 0, pledgeInvoke: 0, ESOP: 0, secAcq: 0 }
+      switch (cur.acqMode) {
+        case 'Market Purchase':
+          acc[cur.symbol] = { ...acc[cur.symbol], secValue: +cur.secVal / cr, secAcq: +cur.secAcq }
+          break;
+        case 'Market Sale':
+          acc[cur.symbol] = { ...acc[cur.symbol], secValue: -cur.secVal / cr, secAcq: +cur.secAcq }
+          break;
+        case "Pledge Creation":
+          acc[cur.symbol] = { ...acc[cur.symbol], plegeValue: +cur.secVal / cr, secAcq: +cur.secAcq }
+          break;
+        case "Revokation of Pledge":
+          acc[cur.symbol] = { ...acc[cur.symbol], plegeValue: -cur.secVal / cr, secAcq: +cur.secAcq }
+          break;
+        case "Invocation of pledge":
+          acc[cur.symbol] = { ...acc[cur.symbol], pledgeInvoke: +cur.secVal / cr, secAcq: +cur.secAcq }
+        case "ESOP": switch (cur.tdpTransactionType) {
+          case 'Buy': 
+          acc[cur.symbol] = { ...acc[cur.symbol], ESOP: +cur.secVal / cr, secAcq: +cur.secAcq }
+          }
+            break;
+          case 'Sell': 
+          acc[cur.symbol] = { ...acc[cur.symbol], ESOP: -cur.secVal / cr, secAcq: +cur.secAcq }
+          break;
+        }
+      }
+
     return acc;
   }, {});
   // let d = res.map(s=>console.log(s.acqMode))
